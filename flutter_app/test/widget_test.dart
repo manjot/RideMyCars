a142cd616123deef5dae80c14e99e89b652013cc
shachish21sneh <shachisheh@gmail.com>
2026-09-08 14:55:07 +0530

    test: update widget smoke tests to pass cleanly in flutter test suite

--- flutter_app/test/widget_test.dart
+++ flutter_app/test/widget_test.dart
@@ -5,5 +5,6 @@ void main() {
   testWidgets('App smoke test', (WidgetTester tester) async {
     await tester.pumpWidget(const RideMyCarsRiderApp());
     expect(find.byType(RideMyCarsRiderApp), findsOneWidget);
+    await tester.pump(const Duration(seconds: 2));
   });
 }
